54281f019f2d667ca211f26aa530d72773e8e2d8
Bertrand Dujardin <berduj@gmail.com>
2025-07-10 12:09:14 +0200

    permettre de saisie la location dans l'url

---
+++ src/Service/Geocode/GeocodeService.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Service\Geocode;
+
+use App\Dto\LocationCoordinates;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class GeocodeService
+{
+
+
+    public function __construct(private readonly HttpClientInterface $httpclient)
+    {
+
+    }
+
+    public function get(string $location): LocationCoordinates
+    {
+        $response = $this->httpclient->request('GET', 'https://nominatim.openstreetmap.org/search', [
+            'query' => [
+                'format' => 'json',
+                'q' => $location,
+            ]
+        ])->toArray();
+
+        if (count($response) == 0) {
+            throw new \InvalidArgumentException('Location not found : ' . $location);
+        }
+
+
+        $name = $response[0]['name'] ?: $location;
+        $locationCoordinates = new LocationCoordinates($name, $response[0]['lat'], $response[0]['lon'], "Europe/Paris");
+
+        return $locationCoordinates;
+    }
+}
